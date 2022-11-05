502df850c7dc323d82ab9e42e564736564d6b815
AlenaKarpolenko <alenac190590@mail.ru>
2022-11-05 20:39:42 +0700

    1

--- DB/exchangeRate.js
+++
@@ -1,5 +0,0 @@
-exchangeRate = ['USD: 72,95', 'EUR: 86,57'];
-
-module.exports = {
-    exchangeRate,
-}
--- server.js
+++ server.js
@@ -1,219 +1,102 @@
 const http = require('http');
-const path = require('path');
-const fs = require('fs');
 const Koa = require('koa');
 const koaBody = require('koa-body');
-const  Router = require('koa-router');
-const json = require('koa-json');
 const koaStatic = require('koa-static');
-const { streamEvents } = require('http-event-stream');
-const router = new Router();
+const Router = require('koa-router');
+const cors = require('koa2-cors');
+const WS = require('ws');
+const path = require('path');
+const Storage = require('./Storage');
+
+
 const app = new Koa();
+const router = new Router();
 
-
-const { db } = require('./DB/db');
-const faker = require('faker');
-const { emoji } = require('./DB/emoji');
-
-let initFakeTime = Date.now() - 5000000;
-
-const public = path.join(__dirname, '/public');
-for (let i = 0 ; i < 50 ; i++) {
-  const obj = {
-    type: 'text',
-    data: {
-        content: {
-          text: [faker.lorem.sentence()]
-        },
-        date: initFakeTime,
-    }
-  }
-  db.addNewPosts(obj);
-
-  initFakeTime += 10000;
-}
-
-getInitMediaFiles();
-
-
-app.use( koaBody({
-  urlencoded: true,
-  multipart: true,
-  json: true,
+// Body Parsers
+app.use(koaBody({
+  json: true, text: true, urlencoded: true, multipart: true,
 }));
 
-app.use( koaStatic(public));
+// CORS
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+    'Access-Control-Allow-Origin': true,
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
+  })
+);
 
-app.use(json());
-
-app.use( async (ctx, next) => {
-  const origin = ctx.request.get('Origin');
-  if (!origin) {
-    return await next();
-  }
-
-  const headers = { 'Access-Control-Allow-Origin': '*'};
-
-  if (ctx.request.method !== 'OPTIONS') {
-    console.log('! OPTIONS');
-    ctx.response.set({ ...headers });
-    try {
-      return await next();
-    } catch (e) {
-      e.headers = { ...e.headers, ...headers };
-      throw e;
-    }
-  }
-
-  if (ctx.request.get('Access-Control-Request-Method')) {
-    ctx.response.set({
-      ...headers,
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
-    });
-
-    if (ctx.request.get('Access-Control-Request-Headers')) {
-      ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Request-Headers'));
-    }
-
-    ctx.response.status = 204;
-  }
-})
-
-const port = process.env.PORT || 7070;
-const server = http.createServer(app.callback());
-
-
+// Routers
 app.use(router.routes()).use(router.allowedMethods());
 
-// подключение к серверу
+// Files Directory
+const filesDir = path.join(__dirname, '/files');
+app.use(koaStatic(filesDir));
 
-router.get('/sse', async (ctx) => {
-  streamEvents(ctx.req, ctx.res, {
-    async fetch(lastEventId) {
-      console.log(lastEventId);
-      return [];
-    },
-    stream(sse) {
-      db.listen( item => {
-        sse.sendEvent({
-          data: JSON.stringify(item),
-        });
-      });
-      return () => {};
-    }
-  });
-  db.initSendPosts();
-  ctx.respond = false;
-});
+// Starting Server
+const port = process.env.PORT || 7070;
+const server = http.createServer(app.callback());
+const wsServer = new WS.Server({ server });
 
-// ленивая подгрузка постов
+// DATABASE
+const dB = [
+  {id: '111', message: 'Котойога', date: Date.now() - 500000000, geo: '', type: 'text'},
+  {id: '112', message: 'cat.jpg', date: Date.now() - 250000000, geo: '', type: 'image'},
+  {id: '113', message: 'Впервые идея создания этого направления йоги возникла на Западе. Не так давно практику этих занятий переняли в странах постсоветского пространства. Во многих городах открываются котокафе и фитнес-центры, где проводятся занятия йогой вместе с кошками. На Западе люди приходят со своими питомцами, тогда как у нас больше распространена практика, при которой кошки, взятые из приюта, живут в йога-центрах. Часто люди имеют возможность забрать домой понравившегося питомца – так многие коты находят новых хозяев.', date: Date.now() - 450000000, geo: '', type: 'text'},
+  {id: '114', message: 'Преимущества котойоги', date: Date.now() - 400000000, geo: '', type: 'text'},
+  {id: '115', message: 'Котойога является ответвлением хатха-йоги – пути совершенствования духа через совершенство физическое. Чтобы прочувствовать весь смысл занятий с четвероногими, нужно настроиться на определенную волну, попытаться войти в контакт с животными. Только тогда будет достигнут максимальный эффект, порой превосходящий все ожидания. Питомцы, свободно разгуливающие по залу, способны незначительно ограничить выполнение некоторых асан. Животное может разлечься на коврике для занятий или даже взобраться на спину или другие части тела. Тревожить кошку в таких случаях не принято, иначе ускользнет вся суть занятий котойогой. В целом комплекс упражнений не имеет каких-либо особенностей – асаны выполняются в обычном режиме. Нужно лишь чувствовать кошку, а она будет чувствовать вас, передавая свою энергию.', date: Date.now() - 400000000, geo: '', type: 'text'},
+  {id: '116', message: 'Как правило, на одного человека приходится в среднем две кошки. Так, в зале, рассчитанном на 10 человек, находятся обычно около 20 кошек.', date: Date.now() - 350000000, geo: '41.013800, 28.949700', type: 'text'},
+  {id: '117', message: 'Котойога – сравнительно новое направление, но оно уже покорило сердца многих. Каждому человеку стоит хотя бы раз попробовать такую практику, которая способна подарить совершенно новый, ни на что не похожий опыт и уникальные впечатления.', date: Date.now(), geo: '', type: 'text'},
+  {id: '117g7i9m-y7gf-3t4y-66i8-uzumymw666ad', message: 'cat_yoga.pdf', date: Date.now() - 100000000, geo: '', type: 'file'},
+  {id: '118', message: 'Время Scatman', date: Date.now(), geo: '', type: 'text'},
+  {id: '221', message: 'cat_scatman.mp4', date: Date.now() - 200000000, type: 'video'},
+  {id: '222', message: 'Время Мяу', date: Date.now(), geo: '', type: 'text'},
+  {id: '223', message: 'meow.mp3', date: Date.now() - 150000000, geo: '41.013800, 28.949700', type: 'audio'},
+  {id: '222gdak6-io98-6i9i-ip0y-hesoyam100he', message: 'Ссылка на изпользованный источник https://101kote.ru/stati/kotoyoga.html, ссылка на видео https://www.youtube.com/watch?v=dCkz1fgZLQo', date: Date.now() - 300000000, geo: '', type: 'text'},
 
-router.get('/previousposts/:id', (ctx) => {
-  const indexLastDownloadedPost = ctx.params.id;
-  const data =  db.getPosts(ctx.params.id);
-  if (data.length === 0) {
-    ctx.response.body = {status: false};
-    return;
-  }
-  ctx.response.body = {status: 'ok', data: data};
-});
 
-// скачивание медиа файла
+];
+const category = {
+  links: [
+    { name: 'https://101kote.ru/stati/kotoyoga.html', messageId: '117g7i9m-y7gf-3t4y-66i8-uzumymw666ad' },
+    { name: 'https://www.youtube.com/watch?v=dCkz1fgZLQo', messageId: '117g7i9m-y7gf-3t4y-66i8-uzumymw666ad' },
+  ],
+  image: [
+    { name: 'cat.jpg', messageId: '112' },
+  ],
+  video: [
+    { name: 'cat_scatman.mp4', messageId: '221' },
+  ],
+  audio: [
+    { name: 'meow.mp3', messageId: '223' },
+  ],
+  file: [
+    { name: 'cat_yoga.pdf', messageId: '117g7i9m-y7gf-3t4y-66i8-uzumymw666ad' },
+  ],
+};
+const favourites = new Set(['112', '117g7i9m-y7gf-3t4y-66i8-uzumymw666ad', '221']);
 
-router.get('/download/:filename', (ctx) => {
-  const filname = ctx.params.filename;
-  const filepath = path.join(public, filname)
-  const readStream = fs.readFileSync(filepath);
-  ctx.response.body = readStream;
-});
 
-// сохранение текстовых данных
+const clients = [];
+wsServer.on('connection', (ws) => {
+  clients.push(ws);
+  const storage = new Storage(dB, category, favourites, filesDir, ws, clients);
+  storage.init();
 
-router.post('/text', (ctx) => {
-    const item = JSON.parse(ctx.request.body);
-    console.log(item);
-    db.addNewPosts(item);
-    ctx.response.body = {status: 'ok'};
-});
-
-// сохранение медиа данных
-
-router.post('/media', async (ctx) => {
-  const file = ctx.request.files.file;
-  const body = JSON.parse(ctx.request.body.textData);
-  const linkSourse = await downloadMedia(file);
-  body.data.content.link = linkSourse;
-  console.log(body)
-  db.addNewPosts(body)
-  ctx.response.body = {status: 'ok'};
-});
-
-// скачивание медиа
-
-async function downloadMedia(file) {
-  const link = await new Promise((resolve, reject) => {
-    const oldPath = file.path;
-    const fileName = file.name;
-    const newPath = path.join(public, fileName);
-
-    const callback = (error) => reject(error);
-
-    const readStream = fs.createReadStream(oldPath);
-    const writeStream = fs.createWriteStream(newPath);
-
-    readStream.on('error', callback);
-    writeStream.on('error', callback);
-
-    writeStream.on('close', () => {
-      fs.unlink(oldPath, callback);
-      console.log('close');
-      resolve(fileName);
+  router.post('/upload', async (ctx) => {
+    storage.loadFile(ctx.request.files.file, ctx.request.body.geo).then((result) => {
+      storage.wsAllSend({ ...result, event: 'file' });
     });
-
-    readStream.pipe(writeStream);
+    ctx.response.status = 204;
   });
-  return link;
-}
 
-// запрос эмоджи
-
-router.get('/emoji', (ctx) => {
-  ctx.response.body = {status: 'ok', data : emoji};
+  ws.on('close', () => {
+    const wsIndex = clients.indexOf(ws);
+    if (wsIndex !== -1) {
+      clients.splice(wsIndex, 1);
+    }
+  });
 });
 
-function getInitMediaFiles() {
-  const obj = {
-      type: 'video',
-      data: { content: { link: 'sample-5s.mp4' },
-      date: initFakeTime
-    }
-  }
-  db.addNewPosts(obj);
-
-  initFakeTime += 10000;
-
-  const image = {
-    type: 'image',
-    data: {
-      content: { link: '372ba3f7831018e824b4e799ed40f281.jpg' },
-      date: initFakeTime
-    }
-  }
-  db.addNewPosts(image);
-
-  initFakeTime += 10000;
-
-  const audio = {
-      type: 'audio',
-      data: { content: { link: 'test.mp3' },
-      date: initFakeTime
-    }
-  }
-
-  db.addNewPosts(audio);
-
-  initFakeTime += 10000;
-}
-
-
-server.listen(port);
+server.listen(port, () => console.log('Server started'));
